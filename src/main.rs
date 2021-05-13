dbfb7bba9c7b54ee206a8c3fef963c6e941f7996
Martin Ankerl <martin.ankerl@gmail.com>
2021-05-13 19:03:42 +0200

    playing with command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
+// arguments:
+// throttler tagname a b c ...
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("throttler")
+        .version("0.1")
+        .author("Martin Ankerl <martin.ankerl@gmail.com>")
+        .about("Throttles concurrent executions")
+        .arg(
+            Arg::with_name("group")
+                .short("g")
+                .long("group")
+                .value_name("NAME")
+                .default_value("NOGROUP")
+                .help("Specify to which group the command belongs to")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("command")
+                .value_name("COMMAND")
+                .required(true)
+                .multiple(true)
+                .last(true),
+        )
+        .get_matches();
+
+    println!("group: {:?}", matches.value_of("group").unwrap());
+    println!(
+        "'command' values: {:?}",
+        matches
+            .values_of("command")
+            .map(|args| args.collect::<Vec<_>>())
+    );
 }
